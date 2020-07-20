c048a0a5ffea0af2408ad937395ff01c9781da55
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2020-07-20 18:54:00 -0500

    created functions js

--- main.js
+++ main.js
@@ -1,67 +1,67 @@
-console.log("Main.js, yo!")
-
-
-
-
-// let x = true;
-// let y = false;
-
-// let num1 = 1;
-// let num2 = 2;
-
-// let appleTypes = {
-//     type1: 'granny smith',
-//     type2: 'fuji'
-// }
-
-// let howManyApples = '5';
-
-// let appleCost = (qty) => {
-//     console.log(qty * 22)
-// }
-
-// console.log(typeof appleTypes['type1'])
-
-
-
-
-// if (typeof howManyApples !== 'number') {
-//     howManyApples = Number(howManyApples);
-// }
-
-// console.log(Number(howManyApples) + 22);
-// console.log(typeof Number(howManyApples));
-
-
-// console.log(num1 > num2)
-
-
-// if (x) {
-//     console.log("in here!")
-// } else {
-//     console.log("in there!")
-// }
-
-
-
-
-
 // console.log("Main.js, yo!")
 
-// // let name ="Smitty";
-// // let score = 42;
 
-// let name ="Smitty",
-//     score = 42;
 
-// let winner = {
-//     name: name,
-//     score: score
-// }
 
-// let allWinners = {
-//     name,
-//     score
-// }
+// // let x = true;
+// // let y = false;
 
-// console.log(allWinners)
+// // let num1 = 1;
+// // let num2 = 2;
+
+// // let appleTypes = {
+// //     type1: 'granny smith',
+// //     type2: 'fuji'
+// // }
+
+// // let howManyApples = '5';
+
+// // let appleCost = (qty) => {
+// //     console.log(qty * 22)
+// // }
+
+// // console.log(typeof appleTypes['type1'])
+
+
+
+
+// // if (typeof howManyApples !== 'number') {
+// //     howManyApples = Number(howManyApples);
+// // }
+
+// // console.log(Number(howManyApples) + 22);
+// // console.log(typeof Number(howManyApples));
+
+
+// // console.log(num1 > num2)
+
+
+// // if (x) {
+// //     console.log("in here!")
+// // } else {
+// //     console.log("in there!")
+// // }
+
+
+
+
+
+// // console.log("Main.js, yo!")
+
+// // // let name ="Smitty";
+// // // let score = 42;
+
+// // let name ="Smitty",
+// //     score = 42;
+
+// // let winner = {
+// //     name: name,
+// //     score: score
+// // }
+
+// // let allWinners = {
+// //     name,
+// //     score
+// // }
+
+// // console.log(allWinners)
