cd9d40a5b85b8bb7e92f578af9f44bf16ed8d192
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2020-07-20 19:56:56 -0500

    created functions

---
+++ functions.js
@@ -0,0 +1,19 @@
+// console.log("functions")
+
+const nuggetizer = (animal) => {
+    return `processed ${animal}`;
+}
+
+console.log(nuggetizer('pig'));
+console.log(nuggetizer('fish'));
+console.log(nuggetizer('chicken'));
+console.log(nuggetizer('student'));
+
+// Write a function that takes any number and returns that number plus 42
+
+
+// Write a function that takes in the year you were born and figures out how old you will be in 2099
+
+const oldAge = () => {
+
+}
